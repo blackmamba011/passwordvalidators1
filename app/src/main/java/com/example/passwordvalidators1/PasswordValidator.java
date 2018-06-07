78d9d74f1f0bbec9504c97eeb0824a8210dff2b5
blackmamba011 <yadiogbudibe@gmail.com>
2018-06-07 18:29:29 -0300

    Creating new branch With Java Class format JUnit test for Stage 1 Complete and working Java code also involved for password check

--- app/src/main/java/com/example/passwordvalidators1/PasswordValidator.java
+++ app/src/main/java/com/example/passwordvalidators1/PasswordValidator.java
@@ -1,4 +1,43 @@
 package com.example.passwordvalidators1;
 
 public class PasswordValidator {
+    private String pass="ABCDEFGH";
+    private boolean secure=true;
+
+    private String lowp=pass.toLowerCase();
+
+    public void setInfo(String p,boolean s){
+        lowp=p;
+        secure=s;
+    }
+
+
+    public String getPass() {
+        return lowp;
+    }
+
+    public boolean getSecure(){
+        return secure;
+    }
+
+    public boolean isSecure() {
+        return passCheck();
+    }
+
+
+    public boolean passCheck() {
+        if (lowp.length() < 8) {
+            secure = false;
+            return secure;
+        }
+        else if (lowp.equals("password")) {
+            secure = false;
+            return secure;
+        }
+
+        else
+            return secure;
+
+    }
+
 }
---
+++ app/src/test/java/com/example/passwordvalidators1/ValidatorTest.java
@@ -0,0 +1,39 @@
+package com.example.passwordvalidators1;
+
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ValidatorTest {
+    static boolean secure=true;
+    static String pass="ABCDEFGH";
+    static String lowp=pass.toLowerCase();
+
+    static PasswordValidator Validator;
+
+    @BeforeClass
+    public static void init(){
+        Validator=new PasswordValidator();
+        Validator.setInfo(lowp,secure);
+    }
+
+
+    @Test
+    public void passIsCorrect(){
+    assertEquals(lowp, Validator.getPass());
+    }
+    public void secureIsCorrect(){
+    assertEquals(secure,Validator.getSecure());
+    }
+
+    @Test
+    public void checkIsCorrect(){
+        assertEquals(secure, Validator.passCheck());
+    }
+}
