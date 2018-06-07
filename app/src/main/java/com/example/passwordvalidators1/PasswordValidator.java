728bc40c1e516c3d23e34c22454eedd30d85b82b
blackmamba011 <yadiogbudibe@gmail.com>
2018-06-07 19:03:38 -0300

    Creating new branch With Java Class format JUnit test for Stage 1 Complete and working Java code also involved for password check

--- app/src/main/java/com/example/passwordvalidators1/PasswordValidator.java
+++ app/src/main/java/com/example/passwordvalidators1/PasswordValidator.java
@@ -1,10 +1,14 @@
 package com.example.passwordvalidators1;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class PasswordValidator {
     private String pass="ABCDEFGH";
     private boolean secure=true;
 
+
     private String lowp=pass.toLowerCase();
+    private String highp=pass.toUpperCase();
 
     public void setInfo(String p,boolean s){
         lowp=p;
@@ -32,6 +36,21 @@ public class PasswordValidator {
         }
         else if (lowp.equals("password")) {
             secure = false;
+            return secure;
+        }
+        //15 or less
+        else if(pass.length()>15){
+            secure=false;
+            return secure;
+        }
+        //contains a number
+        else if(!(Pattern.compile("[0-9]").matcher(pass).find())){
+            secure=false;
+            return secure;
+        }
+        //capital letter check
+        else if(!(Pattern.compile("[A-Z]").matcher(pass).find())){
+            secure=false;
             return secure;
         }
 
